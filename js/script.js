63c63c166e9ed39e897935197835e54c56f68b5f
tsizehena223 <tsizehena223@gmail.com>
2023-02-02 20:59:58 +0300

    Forme et structure

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/* Typing Animation */
+var typed = new Typed(".typing", {
+    strings: ["", "Web Designer", "Web Developper", "Graphic Designer"],
+    typeSpeed: 100,
+    BackSpeed: 60,
+    loop: true
+})
